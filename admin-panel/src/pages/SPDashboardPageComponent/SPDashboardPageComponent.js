24673a2a0a4eaf89afe127502b7f91d67fcddd5a
vidithw20 <vidithw99@gmail.com>
2022-06-28 14:52:26 +0530

    Service Provider Dashboard

--- admin-panel/src/pages/SPDashboardPageComponent/SPDashboardPageComponent.js
+++ admin-panel/src/pages/SPDashboardPageComponent/SPDashboardPageComponent.js
@@ -1,114 +1,95 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import { Space, Table, Tag } from 'antd';
 import 'antd/dist/antd.css';
 import AdminNavComponent from '../../components/AdminNavComponent/AdminNavComponent';
+import './SPDashboardPageComponent.scss';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import CountUp from 'react-countup';
+import Box from '@mui/material/Box';
 
-const columns = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        render: (text) => <a>{text}</a>,
-    },
-    {
-        title: 'Age',
-        dataIndex: 'age',
-        key: 'age',
-    },
-    {
-        title: 'Address',
-        dataIndex: 'address',
-        key: 'address',
-    },
-    {
-        title: 'Tags',
-        key: 'tags',
-        dataIndex: 'tags',
-        render: (tags) => (
-            <span>
-                {tags.map((tag) => {
-                    let color = tag.length > 11 ? 'geekblue' : tag.length > 9 ?'green': tag.length > 7 ?'volcano': 'purple';
-
-                    if (tag === 'loser') {
-                        color = 'volcano';
-                    }
-
-                    return (
-                        <Tag color={color} key={tag}>
-                            {tag.toUpperCase()}
-                        </Tag>
-                    );
-                })}
-            </span>
-        ),
-    },
-    {
-        title: 'Action',
-        key: 'action',
-        render: (_, record) => (
-            <Space size="middle">
-                <a>Approve</a>
-            </Space>
-        ),
-    },
-];
-const data = [
-    {
-        key: '1',
-        name: 'John Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park',
-        tags: ['developer'],
-    },
-    {
-        key: '2',
-        name: 'Jim Green',
-        age: 42,
-        address: 'London No. 1 Lake Park',
-        tags: ['painter'],
-    },
-    {
-        key: '2',
-        name: 'Jim Green',
-        age: 42,
-        address: 'London No. 1 Lake Park',
-        tags: ['full stack developer'],
-    },
-    {
-        key: '3',
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sidney No. 1 Lake Park',
-        tags: ['teacher'],
-    },
-    {
-        key: '3',
-        name: 'Joe white',
-        age: 32,
-        address: 'Sidney No. 1 Lake Park',
-        tags: ['plumber'],
-    },
-    {
-        key: '3',
-        name: 'kim Black',
-        age: 32,
-        address: 'Sidney No. 1 Lake Park',
-        tags: ['wall artist'],
-    },
-];
+import { getServiceproviders } from '../../services/AuthService';
 
 const SPDashboardPageComponent = () => {
-    return (
-        <div>
-            <AdminNavComponent />
-            <div className='admincategory'>
-                <Table
+  const [serviceproviders, setServiceproviders] = useState([]);
+
+  useEffect(() => {
+    GetServiceproviders();
+  }, []);
+
+  const GetServiceproviders = async () => {
+    try {
+      const response = await getServiceproviders();
+      console.log(response.data.data);
+      setServiceproviders(response.data.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <div>
+      <AdminNavComponent />
+      <div className="admincategory">
+        <div className="service_header">
+          {/* <Table
                     columns={columns}
                     dataSource={data}
-                />
-            </div>
+                /> */}
+          <p>All Service Providers</p>
         </div>
-    )
-}
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          <Grid item xs={12} id="users">
+            <div className="userlist" id="userlist">
+              <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                {/* <Typography component="header" variant="h6"  >
+                                        <Box sx={{ textAlign: 'center', m: 1 }}>Users list</Box>
+                                    </Typography> */}
+                <table>
+                  <thead>
+                    <tr>
+                      <th scope="col">Name</th>
+                      <th scope="col">Category</th>
+                      <th scope="col">Mail</th>
+                      <th scope="col">Reviews</th>
+                      <th scope="col">Approved Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {serviceproviders.map((user) => (
+                      <tr key={user._id}>
+                        <td data-label="Name">
+                          <h6>
+                            <Link to={`/user/${user._id}`} className="linkuser"  style={{color: 'black' ,textDecoration: 'none'}}>
+                              {user.serviceProviderID.name}
+                            </Link>
+                          </h6>
+                        </td>
+                        <td data-label="batch">
+                          <h6>{` ${user.categoryID.name}`}</h6>
+                        </td>
+                        <td data-label="batch">
+                          <h6>{user.serviceProviderID.email}</h6>
+                        </td>
+                        <td data-label="batch">
+                          <h6>4.2</h6>
+                        </td>
+                        <td data-label="batch">
+                          <h6>{user.isApprovedStatus ? "Pending":"Approved"}</h6>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </Paper>
+            </div>
+          </Grid>
+        </Container>
+      </div>
+    </div>
+  );
+};
 
-export default SPDashboardPageComponent
+export default SPDashboardPageComponent;
